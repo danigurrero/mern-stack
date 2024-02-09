2667af57e66608b00da5bbce183c1baa16d679c5
Danyal Ahmad <danyal.ahmad@pef.edu.pk>
2024-02-09 15:10:07 +0500

    first commit

--- Backend/8.3 Travel Tracker/index.js
+++ Backend/8.3 Travel Tracker/index.js
@@ -1,12 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
-import pg from 'pg';
+import pg from "pg";
 
 const app = express();
 const port = 3000;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
 const db = new pg.Client({
   user: "postgres",
@@ -14,40 +11,78 @@ const db = new pg.Client({
   database: "world",
   password: "ABCxyz786",
   port: 5432,
-})
-
+});
 db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 async function checkVisisted() {
   const result = await db.query("SELECT country_code FROM visited_countries");
-
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
   return countries;
 }
-
 // GET home page
 app.get("/", async (req, res) => {
   const countries = await checkVisisted();
   res.render("index.ejs", { countries: countries, total: countries.length });
 });
 
+//INSERT new country
 app.post("/add", async (req, res) => {
-  const formText = req.body["country"]
-  const result = await db.query("Select country_code from countries where country_name = $1", [formText])
+  const input = req.body["country"];
 
-  if (result.rows.length!== 0) {
+  try {
+    // Define two different selection checks
+    const patternMatchReq = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%'",
+      [input.toLowerCase()]
+    );
+
+    const equalMatchReq = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) = $1",
+      [input.toLowerCase()]
+    );
+
+    // "RESULT" IS A CONDITIONAL THAT CHECKS IF THE PATTERN MATCH WILL PRODUCE MULTIPLE ITEMS
+    // IF TRUE, "RESULT" REQUESTS AN EQUAL MATCH
+    const result = patternMatchReq.rows.length > 1 ? equalMatchReq : patternMatchReq;
+
     const data = result.rows[0];
+    if (!data) {
+      throw new Error("Country name does not exist, try again.");
+    }
     const countryCode = data.country_code;
-
-    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
-      countryCode,
-    ]);
-    res.redirect("/");
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [countryCode]
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again.",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: err.message,
+    });
   }
+});
 
-})
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
